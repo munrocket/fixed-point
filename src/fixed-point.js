a924725deac9b71096f1404b3ee1e65cdae01c42
munrocket <munrocket@pm.me>
2021-10-05 23:35:06 +0400

    arithmetic without rounding

--- src/fixed-point.js
+++ src/fixed-point.js
@@ -1,23 +1,60 @@
 export default class FixedPoint {
   static DP = 2;  // digits after point
   static RM = 0;  // rounding mode
-  n;              // bigint number
+  num;            // bigint number
 
-  constructor(m) { this.n = BigInt(m); }
-
-  toString() { return this.n.toString(); }
-  add(m) { return this.n + m; }
-  sub(m) { return this.n - m; }
-
-  mul(m) {
-    if (RM = 0) {
-      return (this.n * m) / (10 ** DP);
+  constructor(obj) {
+    if (typeof obj === 'bigint') {
+      this.num = obj;
+    } else if (typeof obj === 'number') {
+      return FixedPoint.fromString(obj.toFixed(FixedPoint.DP));
+    } else if (typeof obj === 'string') {
+      return FixedPoint.fromString(obj);
+    } else if (obj instanceof FixedPoint) {
+      this.num = obj.num;
     }
   }
 
-  div(m) {
-    if (RM = 0) {
-      return this.n / m;
+  static fromString(s) {
+    const i = s.indexOf('.');
+    if (i == -1) {
+      s += '0'.repeat(FixedPoint.DP);
+    } else {
+      const end = i + 1 + FixedPoint.DP;
+      const tail = end - s.length;
+      if (tail > 0) s += '0'.repeat(tail);
+      s = s.slice(0, i) + s.slice(i + 1, end);
+    }
+    return new FixedPoint(BigInt(s));
+  }
+
+  toFixed() {
+    const s = this.num.toString();
+    const i = s.length - FixedPoint.DP;
+    if (i <= 0) {
+      return '0.' + '0'.repeat(-i) + s;
+    } else {
+      return s.slice(0, i) + '.' + s.slice(i);
     }
   }
+
+  toString() {
+    const s = this.toFixed();
+    return (s.indexOf('.') == -1) ? s : s.replace(/\.?0+$/, '');
+  }
+
+  add(m) { this.num += m.num; return this; }
+  sub(m) { this.num -= m.num; return this; }
+
+  // mul(m) {
+  //   if (RM = 0) {
+  //     return (this.num * this.m) / (10n ** BigInt(FixedPoint.DP));
+  //   }
+  // }
+
+  // div(m) {
+  //   if (RM = 0) {
+  //     return this.num / m;
+  //   }
+  // }
 }
--- test/test.js
+++ test/test.js
@@ -1,7 +1,46 @@
 import FixedPoint from "../src/fixed-point";
-import { test } from 'zora';
+import { hold, report, test } from 'zora';
+import { createDiffReporter } from 'zora-reporters';
+
+hold();
 
 test('constructors', t => {
+  t.ok(new FixedPoint(2n).num == 2n, 'BigInt');
+  t.ok(new FixedPoint(20).num == 2000n, 'Number as integer)');
+  t.ok(new FixedPoint(2.2).num == 220n, 'Number as decimal');
+  t.ok(new FixedPoint(0.01).num == 1n, 'Number as one cent');
+  t.ok(new FixedPoint(0.00).num == 0n, 'Number as zero');
+  t.ok(new FixedPoint(2.2222).num == 222n, 'Number as oveflowed decimal');
+  t.ok(new FixedPoint(0.22222).num == 22n, 'Number as below zero');
+  t.ok(new FixedPoint('2').num == 200n, 'String as integer)');
+  t.ok(new FixedPoint('0.2').num == 20n, 'String as one cent');
+  t.ok(new FixedPoint('2.2').num == 220n, 'String as decimal');
+  t.ok(new FixedPoint('2.2222').num == 222n, 'String as oveflowed decimal');
+  t.ok(new FixedPoint('0.22222').num == 22n, 'String as below zero');
+  t.ok(new FixedPoint(new FixedPoint(2n)).num == 2n, 'FixedPoint');
   FixedPoint.DP = 0;
-  t.ok(new FixedPoint(2).add(new FixedPoint(3)), 5);
+  t.ok(new FixedPoint(2).num == 2n, 'With DP = 0');
+  FixedPoint.DP = 2;
 });
+
+test('toFixed', t => {
+  t.ok(new FixedPoint('20').toFixed() == '20.00', 'integer');
+  t.ok(new FixedPoint('2.2').toFixed() == '2.20', 'decimal');
+  t.ok(new FixedPoint('2.2222').toFixed() == '2.22', 'oveflowed decimal');
+  t.ok(new FixedPoint('0.01').toFixed() == '0.01', 'one cent');
+  t.ok(new FixedPoint('0.22222').toFixed() == '0.22', 'below zero');
+});
+
+test('toString', t => {
+  t.ok(new FixedPoint('20').toString() == '20', 'integer');
+  t.ok(new FixedPoint('2.2').toString() == '2.2', 'decimal');
+  t.ok(new FixedPoint('2.2222').toString() == '2.22', 'oveflowed decimal');
+  t.ok(new FixedPoint('0.01').toFixed() == '0.01', 'one cent');
+});
+
+test('arithmetic without rounding', t => {
+  t.ok((new FixedPoint(10).add(new FixedPoint(0.2))).num == 1020n, 'addition');
+  t.ok((new FixedPoint(0.3).sub(new FixedPoint(0.1))).toString() == '0.2', 'substraction');
+});
+
+report({ reporter: createDiffReporter() });
